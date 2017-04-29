2305f2042b27211257e4eb2f6484384d1433c213
Adri <adri.nemeth92@gmail.com>
2017-04-29 17:09:00 +0200

    implemented new API with AJAX call

--- quoteApp.js
+++ quoteApp.js
@@ -1,17 +1,24 @@
 function loadDoc() {
-var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
-var getQuote = function(data) {
-  $(".quote-text").text('"' + data.quoteText + '"');
-  data.quoteText = encodeURIComponent(data.quoteText);
-  if (data.quoteAuthor === "") {
-    data.quoteAuthor = 'Unknown';
-  }
-  $(".author-text").text("Author: " + data.quoteAuthor);
-  var quot = 'https://twitter.com/intent/tweet?text=' + data.quoteText + 'Author' + ':' + ' ' + data.quoteAuthor;
-  $(".twitter-share-button").attr("href", quot);
-};
+  var getAQuote = function () {
+    $('h3').append('<i class="fa fa-spinner fa-spin fa-3x">');
+    $.ajax({
+      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=movies',
+      type: 'POST', // The HTTP Method
+      data: {}, // Additional parameters here
+      datatype: 'json',
+      success: function (data) {
+        var obj = jQuery.parseJSON(data);
+        $('h3').empty();
+        $(".quote-text").text("\"" + obj.quote + "\"");
+        $(".author-text").text("-" + obj.author);
+        var quot = 'https://twitter.com/intent/tweet?text=' + obj.quote + ' -' + ' ' + obj.author;
+        $(".twitter-share-button").attr("href", quot);
+      },
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader("X-Mashape-Authorization", "2CTPh6fC8XmshZG7Fsc2FVeJYtuRp11F7pAjsnjaHvz9WCZJKo"); // Enter here your Mashape key
+      }
 
-$("#quote").click(function() {
-  $.getJSON(url, getQuote);
-});
+    });
+  }
+  getAQuote();
 };
